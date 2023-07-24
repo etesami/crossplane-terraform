faed5d95fbed845da3131c2f857e85c9044d4500
Ehsan Etesami <eh.etesami@gmail.com>
2023-07-24 00:56:56 +0000

    Upload sample terraform files

---
+++ main.tf
@@ -0,0 +1,21 @@
+module "controlplane" {
+  source           = "remche/rke2/openstack"
+  cluster_name     = var.cluster_name
+  write_kubeconfig = true
+  image_name       = "ubuntu-20.04-focal-x86_64"
+  flavor_name      = "genX1"
+  public_net_name  = "dmz"
+  rke2_config      = file("server.yaml")
+  manifests_path   = "./manifests"
+  # Fix for https://github.com/rancher/rke2/issues/1113
+  additional_san = ["kubernetes.default.svc"]
+}
+
+module "worker" {
+  source      = "remche/rke2/openstack//modules/agent"
+  image_name  = "ubuntu-20.04-focal-x86_64"
+  nodes_count = 2
+  name_prefix = "worker"
+  flavor_name = "genX1"
+  node_config = module.controlplane.node_config
+}
