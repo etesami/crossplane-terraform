e827f6003c5f039d5bd9efbd7be9a706f30b85bb
Ehsan Etesami <eh.etesami@gmail.com>
2023-07-25 03:03:27 +0000

    Add provider block and add variables

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
-terraform {
-  required_providers {
-    openstack = {
-      source = "terraform-provider-openstack/openstack"
-    }
-  }
-  required_version = ">= 0.13.1"
+# terraform {
+#   required_providers {
+#     openstack = {
+#       source = "terraform-provider-openstack/openstack"
+#     }
+#   }
+#   required_version = ">= 0.13.1"
+# }
+
+provider "openstack" {
+  auth_url            = var.openstack_auth_url
+  tenant_name         = var.openstack_tenant_name
+  user_name           = var.openstack_user_name
+  password            = var.openstack_password
+  region              = var.openstack_region
+  user_domain_name    = var.openstack_user_domain_name
+  project_domain_name = var.openstack_project_domain_name
 }
 
 resource "openstack_compute_instance_v2" "my_instance" {
@@ -19,6 +29,62 @@ resource "openstack_compute_instance_v2" "my_instance" {
     name = var.network_name
   }
 
+}
+
+variable "openstack_auth_url" {
+  type = string
+}
+
+variable "openstack_tenant_name" {
+  type = string
+}
+
+variable "openstack_user_name" {
+  type = string
+}
+
+variable "openstack_password" {
+  type = string
+}
+
+variable "openstack_region" {
+  type = string
+}
+
+variable "openstack_user_domain_name" {
+  type = string
+}
+
+variable "openstack_project_domain_name" {
+  type = string
+}
+
+varable "vm_name" {
+  type = string
+} 
+
+variable "region" {
+  type = string
+}
+
+variable "image_id" {
+  type = string
+}
+
+variable "flavor_id" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "security_groups" {
+  type = list(string)
+}
+
+variable "network_name" {
+  type = string
 }
 
 # module "keypair" {
