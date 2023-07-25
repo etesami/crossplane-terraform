3d4a88bf490a82be4ce0ded463cb95d12efd32d0
Ehsan Etesami <eh.etesami@gmail.com>
2023-07-25 04:15:12 +0000

    Uncomment provider block in versions.tf

--- versions.tf
+++ versions.tf
@@ -8,12 +8,12 @@ terraform {
 }
 
 # Configure the OpenStack Provider
-# provider "openstack" {
-#   auth_url           = var.auth_url
-#   tenant_name        = var.tenant_name
-#   user_name          = var.user_name
-#   password           = var.password
-#   region             = var.region
-#   user_domain_name   = var.user_domain_name
-#   project_domain_name = var.project_domain_name
-# }
+provider "openstack" {
+  auth_url           = var.auth_url
+  tenant_name        = var.tenant_name
+  user_name          = var.user_name
+  password           = var.password
+  region             = var.region
+  user_domain_name   = var.user_domain_name
+  project_domain_name = var.project_domain_name
+}
