fd7404ec3ce40904bed83db65d5f0305281d9b94
Ehsan Etesami <eh.etesami@gmail.com>
2023-07-25 03:30:39 +0000

    Remove all provider blocks from the module

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    openstack = {
-      source = "terraform-provider-openstack/openstack"
-    }
-  }
-  required_version = ">= 0.13.1"
-}
+# terraform {
+#   required_providers {
+#     openstack = {
+#       source = "terraform-provider-openstack/openstack"
+#     }
+#   }
+#   required_version = ">= 0.13.1"
+# }
 
 # provider "openstack" {
 #   auth_url            = var.openstack_auth_url
@@ -31,61 +31,7 @@ resource "openstack_compute_instance_v2" "my_instance" {
 
 }
 
-variable "openstack_auth_url" {
-  type = string
-}
 
-variable "openstack_tenant_name" {
-  type = string
-}
-
-variable "openstack_user_name" {
-  type = string
-}
-
-variable "openstack_password" {
-  type = string
-}
-
-variable "openstack_region" {
-  type = string
-}
-
-variable "openstack_user_domain_name" {
-  type = string
-}
-
-variable "openstack_project_domain_name" {
-  type = string
-}
-
-variable "vm_name" {
-  type = string
-} 
-
-variable "region" {
-  type = string
-}
-
-variable "image_id" {
-  type = string
-}
-
-variable "flavor_id" {
-  type = string
-}
-
-variable "key_name" {
-  type = string
-}
-
-variable "security_groups" {
-  type = list(string)
-}
-
-variable "network_name" {
-  type = string
-}
 
 # module "keypair" {
 #   source           = "./modules/keypair"
---
+++ variables.tf
@@ -0,0 +1,55 @@
+variable "openstack_auth_url" {
+  type = string
+}
+
+variable "openstack_tenant_name" {
+  type = string
+}
+
+variable "openstack_user_name" {
+  type = string
+}
+
+variable "openstack_password" {
+  type = string
+}
+
+variable "openstack_region" {
+  type = string
+}
+
+variable "openstack_user_domain_name" {
+  type = string
+}
+
+variable "openstack_project_domain_name" {
+  type = string
+}
+
+variable "vm_name" {
+  type = string
+} 
+
+variable "region" {
+  type = string
+}
+
+variable "image_id" {
+  type = string
+}
+
+variable "flavor_id" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "security_groups" {
+  type = list(string)
+}
+
+variable "network_name" {
+  type = string
+}
