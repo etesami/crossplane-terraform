d54745f34c8d68aa18f23be0775e561009c35bee
Ehsan Etesami <eh.etesami@gmail.com>
2023-07-26 23:57:55 +0000

    Add default values for network name and subnet if not provided

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,17 +1,17 @@
 resource "openstack_networking_network_v2" "nodes_net" {
   count                 = var.create_network ? 1 : 0
-  name                  = var.network_name
+  name                  = try(var.network_name, "${var.cluster_name}-net")
   admin_state_up        = "true"
 }
 
 resource "openstack_networking_subnet_v2" "nodes_subnet" {
   count           = var.create_network ? 1 : 0
-  name            = var.subnet_name
+  name            = try(var.subnet_name, "${var.cluster_name}-subnet")
   network_id      = openstack_networking_network_v2.nodes_net[0].id
-  cidr            = var.network_cidr
+  cidr            = try(var.network_cidr, "10.30.199.0/24")
   ip_version      = 4
   dns_nameservers = ["8.8.8.8", "8.8.4.4"]
-  gateway_ip      = var.network_gateway_ip
+  gateway_ip      = try(var.network_gateway_ip, "10.30.199.1")
 }
 
 resource "openstack_networking_router_interface_v2" "router_interface" {
