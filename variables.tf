4ecbae1215be81413963c9fa73257ab6b65c642c
Ehsan Etesami <eh.etesami@gmail.com>
2023-07-26 15:33:00 +0000

    Remove variables for testing

--- variables.tf
+++ variables.tf
@@ -1,51 +1,51 @@
-variable "auth_url" {
-  type = string
-}
+# variable "auth_url" {
+#   type = string
+# }
 
-variable "tenant_name" {
-  type = string
-}
+# variable "tenant_name" {
+#   type = string
+# }
 
-variable "user_name" {
-  type = string
-}
+# variable "user_name" {
+#   type = string
+# }
 
-variable "password" {
-  type = string
-}
+# variable "password" {
+#   type = string
+# }
 
-variable "region" {
-  type = string
-}
+# variable "region" {
+#   type = string
+# }
 
-variable "user_domain_name" {
-  type = string
-}
+# variable "user_domain_name" {
+#   type = string
+# }
 
-variable "project_domain_name" {
-  type = string
-}
+# variable "project_domain_name" {
+#   type = string
+# }
 
-variable "vm_name" {
-  type = string
-} 
+# variable "vm_name" {
+#   type = string
+# } 
 
-variable "image_id" {
-  type = string
-}
+# variable "image_id" {
+#   type = string
+# }
 
-variable "flavor_id" {
-  type = string
-}
+# variable "flavor_id" {
+#   type = string
+# }
 
-variable "key_name" {
-  type = string
-}
+# variable "key_name" {
+#   type = string
+# }
 
-variable "security_groups" {
-  type = list(string)
-}
+# variable "security_groups" {
+#   type = list(string)
+# }
 
-variable "network_name" {
-  type = string
-}
+# variable "network_name" {
+#   type = string
+# }
