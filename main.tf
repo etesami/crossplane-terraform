5a408f48982eba7683731cb10832c88bf871f1c8
Ehsan Etesami <eh.etesami@gmail.com>
2023-07-26 15:26:40 +0000

    Add modules structure

--- main.tf
+++ main.tf
@@ -1,16 +1,18 @@
 
-resource "openstack_compute_instance_v2" "my_instance" {
-  name            = var.vm_name
-  region          = var.region
-  image_id        = var.image_id
-  flavor_id       = var.flavor_id
-  key_pair        = var.key_name
-  security_groups = var.security_groups
+# resource "openstack_compute_instance_v2" "my_instance" {
+#   name            = var.vm_name
+#   region          = var.region
+#   image_id        = var.image_id
+#   flavor_id       = var.flavor_id
+#   key_pair        = var.key_name
+#   security_groups = var.security_groups
 
-  network {
-    name = var.network_name
-  }
+#   network {
+#     name = var.network_name
+#   }
 
+# }
+
+module "keypair" {
+  source = "./modules/keypair"
 }
-
-
