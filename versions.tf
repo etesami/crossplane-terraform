f28fc300834889b77916c7d1965d26cf797a44ad
Ehsan Etesami <eh.etesami@gmail.com>
2023-07-29 21:07:14 +0000

    Comment out provider block in root verisions.tf for crossplane

--- versions.tf
+++ versions.tf
@@ -7,12 +7,12 @@ terraform {
   required_version = ">= 0.13.1"
 }
 
-provider "openstack" {
-  auth_url           = var.auth_url
-  tenant_name        = var.tenant_name
-  user_name          = var.user_name
-  password           = var.password
-  region             = var.region
-  user_domain_name   = var.user_domain_name
-  project_domain_name = var.project_domain_name
-}
+# provider "openstack" {
+#   auth_url           = var.auth_url
+#   tenant_name        = var.tenant_name
+#   user_name          = var.user_name
+#   password           = var.password
+#   region             = var.region
+#   user_domain_name   = var.user_domain_name
+#   project_domain_name = var.project_domain_name
+# }
