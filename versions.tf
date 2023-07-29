0c2c45fe4e0a0f5f00bb86af377386153f4d9603
Ehsan Etesami <eh.etesami@gmail.com>
2023-07-29 21:21:50 +0000

    Comment trafform block and add provider block only to root

--- versions.tf
+++ versions.tf
@@ -1,18 +1,18 @@
-terraform {
-  required_providers {
-    openstack = {
-      source = "terraform-provider-openstack/openstack"
-    }
-  }
-  required_version = ">= 0.13.1"
-}
-
-# provider "openstack" {
-#   auth_url           = var.auth_url
-#   tenant_name        = var.tenant_name
-#   user_name          = var.user_name
-#   password           = var.password
-#   region             = var.region
-#   user_domain_name   = var.user_domain_name
-#   project_domain_name = var.project_domain_name
+# terraform {
+#   required_providers {
+#     openstack = {
+#       source = "terraform-provider-openstack/openstack"
+#     }
+#   }
+#   required_version = ">= 0.13.1"
 # }
+
+provider "openstack" {
+  auth_url           = var.auth_url
+  tenant_name        = var.tenant_name
+  user_name          = var.user_name
+  password           = var.password
+  region             = var.region
+  user_domain_name   = var.user_domain_name
+  project_domain_name = var.project_domain_name
+}
